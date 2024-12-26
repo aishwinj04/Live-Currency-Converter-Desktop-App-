f54b219a68ff395a61a2e0ddf6ebed26a4610b06
aishwinj04 <aishwin@gmail.com>
2024-12-25 23:35:33 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel, QPushButton, QLineEdit
 
 
+# function to get converted rate
+def get_conversion():
+    return 0
+
+
 app = QApplication([])
 window = QWidget()
 window.setWindowTitle("Currency App")
@@ -19,22 +24,19 @@ layout = QVBoxLayout()
 
 
 # validate button 
+btn = QPushButton('Calculate')
+layout.addWidget(btn)
+btn.clicked.connect(get_conversion)
+
+
+# output value
+output_label = QLabel('')
+layout.addWidget(output_label)
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+# output
 window.setLayout(layout)
 window.show()
 app.exec()
