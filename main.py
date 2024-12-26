60e0e5da8a68bc4165d31fdfc9937c52529ab5d8
aishwinj04 <aishwin@gmail.com>
2024-12-26 00:17:30 -0500

    Update main.py
    BeautifulSoup to get exchange rate

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
 from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel, QPushButton, QLineEdit
+from bs4 import BeautifulSoup
+import requests 
+
+# beautifulsoup to get live exchange rate 
+def get_exchange(input_curr='CAD', output_curr='USD'):
+    url = f"https://www.x-rates.com/calculator/?from={input_curr}&to={output_curr}&amount={amount}"
+    content = requests.get(url).text
+    soup = BeautifulSoup(content, 'html.parser')
+    rate = soup.find("span", class_="ccOutputRslt")
+    rate = float(rate[:-4]) # exlcude code
+
+    return rate
+    
 
 
-# function to get converted rate
-def get_conversion():
-    return 0
+# slot function
+def show_conversion():
+    input_amount = amount.text()
+
+
+
+
 
 
 app = QApplication([])
@@ -21,12 +38,14 @@ layout = QVBoxLayout()
 
 
 # amount entry 
+amount = QLineEdit()
+layout.addWidget(amount)
 
 
 # validate button 
-btn = QPushButton('Calculate')
+btn = QPushButton('Convert')
 layout.addWidget(btn)
-btn.clicked.connect(get_conversion)
+btn.clicked.connect(show_conversion)
 
 
 # output value
